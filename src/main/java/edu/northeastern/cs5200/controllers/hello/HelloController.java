7d6e2ca3a35190829a20d86793a2f88aed5cf038
amitmangotra06 <amitmangotra06@ccs.neu.edu>
2018-02-02 01:33:29 -0500

    Integrating Spring Boot with MySQL

--- src/main/java/edu/northeastern/cs5200/controllers/hello/HelloController.java
+++ src/main/java/edu/northeastern/cs5200/controllers/hello/HelloController.java
@@ -1,10 +1,18 @@
 package edu.northeastern.cs5200.controllers.hello;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
 @RestController
 public class HelloController {
+	
+	@Autowired
+	HelloRepository helloRepo;
 	
 	@RequestMapping("/api/hello/string")
 	public String sayHello() {
@@ -15,5 +23,25 @@ public class HelloController {
 	public HelloObject sayHelloObject() {
 		HelloObject ob = new HelloObject("Hello Amit Mangotra!");
 		return ob;
+	}
+	
+	@RequestMapping("/api/hello/insert")
+	public HelloObject insertHelloObject() {
+		HelloObject obj = new HelloObject("Hello Amit Mangotra!");
+		helloRepo.save(obj);
+		return obj;
+	}
+	
+	@RequestMapping("/api/hello/insert/{msg}")
+	public HelloObject insertessage(@PathVariable("msg") String message) {
+		HelloObject obj = new HelloObject(message);
+		helloRepo.save(obj);
+		return obj;
+	}
+	
+	@RequestMapping("/api/hello/select/all")
+	public List<HelloObject> selectAllHelloObjects() {
+		List<HelloObject> hellos = (List<HelloObject>) helloRepo.findAll();
+		return hellos;
 	}
 }
--- src/main/java/edu/northeastern/cs5200/controllers/hello/HelloObject.java
+++ src/main/java/edu/northeastern/cs5200/controllers/hello/HelloObject.java
@@ -1,21 +1,36 @@
 package edu.northeastern.cs5200.controllers.hello;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity(name="hello")
 public class HelloObject {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	private String message;
 	
-	 private String message;
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getMessage() {
+		return message;
+	}
 	 
-	 public String getMessage() {
-	  return message;
-	 }
+	public void setMessage(String message) {
+		this.message = message;
+	}
 	 
-	 public void setMessage(String message) {
-	  this.message = message;
-	 }
+	public HelloObject(String message) {
+		this.message = message;
+	}
 	 
-	 public HelloObject(String message) {
-	  this.message = message;
-	 }
-	 
-	 public HelloObject() {
-	 }
+	public HelloObject() {
+	}
 }
